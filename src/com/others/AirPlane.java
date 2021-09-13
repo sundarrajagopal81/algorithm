948d433a94857b44442e7faa29d367d201e8a2ed
Mercy <mercypremaranim@gmail.com>
2021-09-12 23:39:30 -0700

    add airplane

---
+++ src/com/others/AirPlane.java
@@ -0,0 +1,27 @@
+//package com.others;
+//
+//import java.io.IOException;
+//
+// class AirPlane {
+//    public AirPlane() throws Exception{
+//
+//
+//        System.out.print("AirJet");
+//        throw new IOException();
+//    }
+//
+//   // class AirJet extends AirPlane{
+//
+//        //public AirJet() throws IOException{
+//
+//            System.out.println("AirJet");
+////            try {
+////            //    super();
+////            }catch (IOException e){
+////                System.out.println("IOEX");
+////            }
+//
+//
+//        }
+//    }
+//}
